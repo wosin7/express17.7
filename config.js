b832509e55137ab178044e44210a747a0dc0edd1
przemek <wosin7@gmail.com>
2018-09-19 18:39:04 +0200

    zadanie

---
+++ config.js
@@ -0,0 +1,6 @@
+ 
+module.exports = {
+	'GOOGLE_CLIENT_ID': '80692805958-07cbre7fnorts9hhibrhcohv4vqah2m0.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'uK6IxVXftL35H-3z4OH-cxg8',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
